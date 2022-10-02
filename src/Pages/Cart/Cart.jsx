960327c805d7141e6dce34f1d5bc5095cee09076
marsan89 <martinsanchez89@gmail.com>
2022-10-02 17:33:06 -0300

    Desafio Item Collecion II

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -2,16 +2,37 @@ import {useContext, useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
 import styles from '../../Pages/Cart/Cart.module.css'
+import { collection, addDoc, getFirestore } from 'firebase/firestore'
+import moment from 'moment'
 
 const Cart = () => {
-  const {cart, deleteCart, removeToCart} = useContext(CartContext)
+  const {cart, deleteCart, removeToCart, setCart, setCounter} = useContext(CartContext)
   const [total, setTotal] = useState(0)
+  const [idCompra, setIdCompra] = useState('')
 
+  const createOrder =() => {
+    const db = getFirestore();
+    const order = {
+      buyer: {
+        name: "Ramiro",
+        phone: "1234123",
+        email: 'q@q.com'
+      },
+      items: cart,
+      total: total,
+      date: moment().format()
+    };
+    const query = collection(db, 'orders');
+    addDoc(query,order)
+    .then(({id}) => setIdCompra(id))
+    .catch(err => console.log(err));
+
+  }
   useEffect(()=>{
     setTotal(cart.reduce((acc, i) => {
       return acc += (i.item.price * i.quantity)}, 0))
   },[cart])  
-    
+
   return (
     <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
       <h1 style={{textAlign:'center', marginTop:'1rem'}}>Cart of mallets! </h1>
@@ -70,10 +91,21 @@ const Cart = () => {
               <button  onClick={()=>deleteCart()} className={`${styles.btnStore}`}>Delete Cart</button>
             </div>
             <div style={{width:'100%', margin:'auto'}}>
-              <button  onClick={()=> (alert('compra finalizada'))} className={`${styles.btnStore}`}>Buy mallets</button>
+              <button  onClick={createOrder} className={`${styles.btnStore}`}>Buy mallets</button>
             </div>
 
           </div>
+          {idCompra != '' && 
+          <div style={{marginTop:'3rem', width:'40%'}}>
+            <div style={{border:'1px solid', padding:'1rem'}}>
+              <h2 style={{textAlign:'center'}}>Purchase of products made</h2>
+              <div className={`${styles.compraRealizada}`}>
+                <h3>Purchase Id </h3>
+                <p style={{margin:'1rem 0'}}>{idCompra} </p>
+              </div>
+            </div>
+              <Link to={"/"} className={`${styles.btnStore}`} onClick={()=>{setCart([]), setCounter(0)}}>Start a new pruchase</Link>
+          </div> }
         </>
       ) }
     </div>
